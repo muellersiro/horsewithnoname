b0cab6b41459eb653328e439b4b9b65bc718c634
Siro Müller <siro.mueller@inpeek.ch>
2018-10-20 10:51:11 +0200

    Inital

---
+++ src/gruppenArbeit01/Collection.java
@@ -0,0 +1,55 @@
+/*
+* Gruppenarbeit 01: Warenkorb
+* Klasse 1o
+* Müller, Siro; Tommasi Nicola
+*
+*/
+package gruppenArbeit01;
+
+import java.util.ArrayList;
+
+public class Collection {
+
+	// product list
+	protected ArrayList<Product> collection = new ArrayList<Product>();
+	// same currency for all products
+	protected String currency;
+
+	public Collection(String currency) {
+		super();
+		this.currency = currency;
+	}
+
+	public void addProduct(Product item) {
+		collection.add(item);
+	}
+
+	public Product getProduct(int id) {
+		int exists = 0;
+		Product item = null;
+		for(int i = 0; i < collection.size(); i++) {
+			item = collection.get(i);
+			if (item.getId() == id) {
+				exists = 1;
+				break;
+			}
+        }
+		if(exists == 0) {
+			return null;
+		}else {
+			return item;
+		}
+	}
+
+	public static int parseToInt(String input) {
+		try {
+			int parsedInt = Integer.parseInt(input);
+			return parsedInt;
+		} catch (NumberFormatException e) {
+			System.out.println("Eingabe ist kein Integer");
+			return -1;
+		}
+
+	}
+
+}
---
+++ src/gruppenArbeit01/ShoppingCart.java
@@ -0,0 +1,64 @@
+/*
+* Gruppenarbeit 01: Warenkorb
+* Klasse 1o
+* Müller, Siro; Tommasi Nicola
+*
+*/
+
+package gruppenArbeit01;
+
+public class ShoppingCart extends Collection {
+
+	public ShoppingCart(String currency) {
+		super(currency);
+
+	}
+
+	public void addItem(Product assortmentItem, int quantity) {
+		assortmentItem.setQuantity(quantity);
+		super.addProduct(assortmentItem);
+	}
+
+	public void updateItem(Product assortmentItem, Product shoppingCartItem, int quantity) {
+		assortmentItem.setQuantity(shoppingCartItem.getQuantity() + quantity);
+		assortmentItem.setPrice(shoppingCartItem.getPrice() + assortmentItem.getPrice());
+		Product item = null;
+		int i =0;
+		for(i = 0; i < collection.size(); i++) {
+			item = collection.get(i);
+			if (item.getId() == assortmentItem.getId()) {
+				break;
+			}
+        }		
+		collection.set(i, assortmentItem);
+		
+	}
+
+	public void showContent() {
+		System.out.println("------------------------------------------------------------");
+		float gesPrice = 0f;
+		float gesMwst = 0f;
+		float gesMwstRate = 0f;
+		if (super.collection.size() > 0) {
+			System.out.println("Ihr Warenkorb:");
+			for (Product item : collection) {
+				float price = item.getPrice() * item.getQuantity();
+				float mwst = item.getMwst() * item.getQuantity();
+				//Sum for each product
+				System.out.println(item.getQuantity() + "\t" + item.getItemName() + "\t" + Product.formatfloats(item.getPrice()) + " "
+						+ super.currency + "\t" + Product.formatfloats(price) + " " + super.currency);
+				gesPrice += price;
+				gesMwst += mwst;
+				gesMwstRate += item.getMwstrate();
+			}
+			//summ overall
+			System.out.println("\n" + "Der Gesamtpreis von " + Product.formatfloats(gesPrice) + " CHF" + " enthält " + Product.formatfloats(gesMwst) + " "
+					+ super.currency + " MwSt (" + gesMwstRate + " %)");
+		} else {
+
+			System.out.println("Ihr Warenkorb ist leer.");
+		}
+
+	}
+
+}
